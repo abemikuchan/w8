3e64c2c9be739395f41d53863cffcaea2449ac4d
abemikuchan <113569221+abemikuchan@users.noreply.github.com>
2022-10-27 14:41:40 +0900

    Update kadaic.js

--- kadaic.js
+++ kadaic.js
@@ -1,18 +1,39 @@
-function start(){
-  
-  window.setInterval(clock, 1000)
-}
-function clock(){
-    let now = new Date();
-    let hh = now.getHours();
-    let mm = now.getMinutes();
-    let ss = now.getSeconds();
-    
-    hh = hh<10? "0" + hh:hh;
-    mm = mm<10? "0" + mm:mm;
-    ss = ss<10? "0" + ss:ss;
-    
-    let result = document.getElementById("result").innerHTML;
-    
-    result.textCountment = hh + ":" + mm + ":" + ss;
-}
+const clock = () => {
+  // 現在の日時・時刻の情報を取得
+  const d = new Date();
+
+  // 年を取得
+  let year = d.getFullYear();
+  // 月を取得
+  let month = d.getMonth() + 1;
+  // 日を取得
+  let date = d.getDate();
+  // 曜日を取得
+  let dayNum = d.getDay();
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let day = weekday[dayNum];
+  // 時を取得
+  let hour = d.getHours();
+  // 分を取得
+  let min = d.getMinutes();
+  // 秒を取得
+  let sec = d.getSeconds();
+
+  // 1桁の場合は0を足して2桁に
+  month = month < 10 ? "0" + month : month;
+  date = date < 10 ? "0" + date : date;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  // 日付・時刻の文字列を作成
+  let today = `${year}.${month}.${date} ${day}`;
+  let time = `${hour}:${min}:${sec}`;
+
+  // 文字列を出力
+  document.querySelector(".clock-date").innerText = today;
+  document.querySelector(".clock-time").innerText = time;
+};
+
+// 1秒ごとにclock関数を呼び出す
+setInterval(clock, 1000);
