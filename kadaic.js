9c27f3eb2d85bea7aec824e780bfb4cb4e4d6259
abemikuchan <113569221+abemikuchan@users.noreply.github.com>
2022-10-27 13:49:14 +0900

    Update kadaic.js

--- kadaic.js
+++ kadaic.js
@@ -1,1 +1,19 @@
-
+function start(){
+  
+  window.setInterval(function()
+  {
+    let now = new Date();
+    let hh = now.getHours();
+    let mm = now.getMinutes();
+    let ss = now.getSeconds();
+    
+    hh = hh<10? "0" + hh:hh;
+    mm = mm<10? "0" + mm:mm;
+    ss = ss<10? "0" + ss:ss;
+    
+    let result = document.getElementById("result");
+    
+    resut.textCountment = hh + ":" + mm + ":" + ss;
+  }
+  , 1000);
+}
